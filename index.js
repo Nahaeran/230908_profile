2ee5f3a207b5dc98fa8cc0d5c7df334141e2c5c6
nahaeran <nh2545@naver.com>
2023-09-08 17:57:55 +0900

    230908 자기소개 페이지

---
+++ index.js
@@ -0,0 +1,4 @@
+const haeran = document.getElementById("haeran")
+const introbtn = document.getElementById("introbtn")
+haeran.addEventListener("click", window.scrollTo(0, 0))
+introbtn.addEventListener("click", window.scrollTo(0, 0))
